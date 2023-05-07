d763b091ccd9442abb6b7a4dec7edce115bc49fa
Yota Toyama <raviqqe@gmail.com>
2023-05-07 17:20:11 +1000

    Fix

--- autophagy/src/instructions.rs
+++ autophagy/src/instructions.rs
@@ -2,3 +2,23 @@
 pub fn add(x: usize, y: usize) -> usize {
     x + y
 }
+
+#[autophagy_macro::instruction(crate = "crate")]
+pub fn sub(x: usize, y: usize) -> usize {
+    x - y
+}
+
+#[autophagy_macro::instruction(crate = "crate")]
+pub fn mul(x: usize, y: usize) -> usize {
+    x * y
+}
+
+#[autophagy_macro::instruction(crate = "crate")]
+pub fn div(x: usize, y: usize) -> usize {
+    x / y
+}
+
+#[autophagy_macro::instruction(crate = "crate")]
+pub fn r#mod(x: usize, y: usize) -> usize {
+    x % y
+}
