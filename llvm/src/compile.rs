782971bc2e0d9f1e4a97946b223ae3867a1eac13
Yota Toyama <raviqqe@gmail.com>
2023-05-07 21:44:01 +1000

    Fix LLVM

--- llvm/src/compile.rs
+++ llvm/src/compile.rs
@@ -1,6 +1,6 @@
 use crate::MlirError;
 use autophagy::Instruction;
-use melior::ExecutionEngine;
+use inkwell::execution_engine::ExecutionEngine;
 
 pub fn compile(_engine: &ExecutionEngine, _instruction: &Instruction) -> Result<(), MlirError> {
     todo!();
