3b0699c3c81a05f965b76b1e5b3c575dabd860fb
Yota Toyama <raviqqe@gmail.com>
2023-05-07 21:40:59 +1000

    Add LLVM backend

---
+++ llvm/src/compile.rs
@@ -0,0 +1,7 @@
+use crate::MlirError;
+use autophagy::Instruction;
+use melior::ExecutionEngine;
+
+pub fn compile(_engine: &ExecutionEngine, _instruction: &Instruction) -> Result<(), MlirError> {
+    todo!();
+}
