03af33c273d0901a57ab6abb902fac41239d8c16
Yota Toyama <raviqqe@gmail.com>
2023-05-07 15:37:30 +1000

    Fix

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -1,8 +1,5 @@
-mod any;
 mod attribute_list;
-mod bindgen;
-mod into_any;
-mod utilities;
+mod instruction;
 
 use self::attribute_list::AttributeList;
 use proc_macro::TokenStream;
@@ -11,33 +8,9 @@ use std::error::Error;
 use syn::{parse_macro_input, ItemFn, ItemStruct};
 
 #[proc_macro_attribute]
-pub fn bindgen(attributes: TokenStream, item: TokenStream) -> TokenStream {
+pub fn instruction(attributes: TokenStream, item: TokenStream) -> TokenStream {
     let attributes = parse_macro_input!(attributes as AttributeList);
     let function = parse_macro_input!(item as ItemFn);
 
     convert_result(bindgen::generate(&attributes, &function))
-}
-
-#[proc_macro_attribute]
-pub fn any(attributes: TokenStream, item: TokenStream) -> TokenStream {
-    let attributes = parse_macro_input!(attributes as AttributeList);
-    let type_ = parse_macro_input!(item as ItemStruct);
-
-    convert_result(any::generate(&attributes, &type_))
-}
-
-#[proc_macro_attribute]
-pub fn into_any(attributes: TokenStream, item: TokenStream) -> TokenStream {
-    let attributes = parse_macro_input!(attributes as AttributeList);
-    let type_ = parse_macro_input!(item as ItemStruct);
-
-    convert_result(into_any::generate(&attributes, &type_))
-}
-
-fn convert_result(result: Result<TokenStream, Box<dyn Error>>) -> TokenStream {
-    result.unwrap_or_else(|error| {
-        let message = error.to_string();
-
-        quote! { compile_error!(#message) }.into()
-    })
 }
