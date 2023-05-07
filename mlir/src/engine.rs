f0cbbf21e7df7cfddf4e409558caa580688cc720
Yota Toyama <raviqqe@gmail.com>
2023-05-07 18:11:52 +1000

    Fix

--- mlir/src/engine.rs
+++ mlir/src/engine.rs
@@ -2,28 +2,77 @@ use crate::MlirError;
 use autophagy::Instruction;
 use melior::ExecutionEngine;
 
-pub struct Engine {
-    engine: ExecutionEngine,
+pub trait Engine {
+    fn add_instruction(&self, _instruction: &Instruction) -> Result<(), MlirError>;
 }
 
-impl Engine {
-    pub fn new(engine: ExecutionEngine) -> Self {
-        Self { engine }
-    }
-
-    pub fn engine(&self) -> &ExecutionEngine {
-        &self.engine
-    }
-
-    pub fn add_instruction(&self, _instruction: &Instruction) -> Result<(), MlirError> {
+impl Engine for ExecutionEngine {
+    fn add_instruction(&self, _instruction: &Instruction) -> Result<(), MlirError> {
         todo!()
     }
+}
 
-    pub unsafe fn run_instruction(
-        &self,
-        name: &str,
-        arguments_and_result: &mut [*mut ()],
-    ) -> Result<(), melior::Error> {
-        self.engine.invoke_packed(name, arguments_and_result)
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use melior::{
+        dialect,
+        ir::Module,
+        pass,
+        utility::{register_all_dialects, register_all_llvm_translations},
+        Context,
+    };
+
+    #[test]
+    fn run() {
+        let registry = dialect::Registry::new();
+        register_all_dialects(&registry);
+
+        let context = Context::new();
+        context.append_dialect_registry(&registry);
+        register_all_llvm_translations(&context);
+
+        let mut module = Module::parse(
+            &context,
+            r#"
+            module {
+                func.func @add(%arg0 : i32) -> i32 attributes { llvm.emit_c_interface } {
+                    %res = arith.addi %arg0, %arg0 : i32
+                    return %res : i32
+                }
+            }
+            "#,
+        )
+        .unwrap();
+
+        let pass_manager = pass::Manager::new(&context);
+        pass_manager.add_pass(pass::conversion::convert_func_to_llvm());
+
+        pass_manager
+            .nested_under("func.func")
+            .add_pass(pass::conversion::convert_arithmetic_to_llvm());
+
+        assert_eq!(pass_manager.run(&mut module), Ok(()));
+
+        let engine = ExecutionEngine::new(&module, 2, &[], false);
+
+        let mut argument = 42;
+        let mut result = -1;
+
+        assert_eq!(
+            unsafe {
+                engine.invoke_packed(
+                    "add",
+                    &mut [
+                        &mut argument as *mut i32 as *mut (),
+                        &mut result as *mut i32 as *mut (),
+                    ],
+                )
+            },
+            Ok(())
+        );
+
+        assert_eq!(argument, 42);
+        assert_eq!(result, 84);
     }
 }
