ea5786bd9d5a4eb363b70daa726819267b9b47ce
Yota Toyama <raviqqe@gmail.com>
2023-05-12 14:47:05 +1000

    Fix

--- mlir/src/compile.rs
+++ mlir/src/compile.rs
@@ -11,7 +11,33 @@ pub fn compile(_module: &Module, instruction: &Instruction) -> Result<(), Error>
 #[cfg(test)]
 mod tests {
     use super::*;
+    use autophagy::math;
+    use melior::{
+        dialect::DialectRegistry,
+        ir::Location,
+        utility::{register_all_dialects, register_all_llvm_translations},
+        Context,
+    };
+
+    fn create_context() -> Context {
+        let registry = DialectRegistry::new();
+        register_all_dialects(&registry);
+
+        let context = Context::new();
+        context.append_dialect_registry(&registry);
+        context.load_all_available_dialects();
+        register_all_llvm_translations(&context);
+
+        context
+    }
 
     #[test]
-    fn add() {}
+    fn add() {
+        let context = create_context();
+
+        let location = Location::unknown(&context);
+        let module = Module::new(location);
+
+        compile(math::add_instruction()).unwrap();
+    }
 }
