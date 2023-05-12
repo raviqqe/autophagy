177900945564287fa5a2b541e12951232d4796a8
Yota Toyama <raviqqe@gmail.com>
2023-05-12 16:04:39 +1000

    Fix

--- mlir/src/compile.rs
+++ mlir/src/compile.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::Error;
 use autophagy::Instruction;
 use melior::{
@@ -5,24 +7,53 @@ use melior::{
     ir::{
         attribute::{IntegerAttribute, StringAttribute, TypeAttribute},
         r#type::{FunctionType, IntegerType},
-        Block, Location, Module, OperationRef, Region, Type,
+        Block, Location, Module, OperationRef, Region, Type, Value,
     },
-    Context,
 };
+
+struct Context<'c> {
+    melior: &'c melior::Context,
+    variables: HashMap<String, Value<'c>>,
+}
+
+impl<'c> Context<'c> {
+    pub fn new(melior: &'c melior::Context) -> Self {
+        Self {
+            melior,
+            variables: Default::default(),
+        }
+    }
+
+    pub fn melior(&self) -> &melior::Context {
+        self.melior
+    }
+
+    pub fn variable(&self, name: &str) -> Result<Value<'c>, Error> {
+        self.variables
+            .get(name)
+            .copied()
+            .ok_or_else(|| Error::VariableNotDefined(name.into()))
+    }
+
+    pub fn insert_variable(&mut self, name: &str, value: Value<'c>) {
+        self.variables.insert(name.to_string(), value);
+    }
+}
 
 pub fn compile(module: &Module, instruction: &Instruction) -> Result<(), Error> {
     let function = instruction.r#fn();
     let context = module.context();
-    let location = Location::unknown(&context);
+    let context = Context::new(&context);
+    let location = Location::unknown(context.melior());
 
     module.body().append_operation(func::func(
-        &context,
-        StringAttribute::new(&context, &function.sig.ident.to_string()),
-        TypeAttribute::new(FunctionType::new(&context, &[], &[]).into()),
+        context.melior(),
+        StringAttribute::new(context.melior(), &function.sig.ident.to_string()),
+        TypeAttribute::new(FunctionType::new(context.melior(), &[], &[]).into()),
         {
             let block = Block::new(&[]);
 
-            compile_block(&context, &block, &function.block)?;
+            compile_block(&context, &block, &function.block, true)?;
 
             let region = Region::new();
             region.append_block(block);
@@ -34,9 +65,14 @@ pub fn compile(module: &Module, instruction: &Instruction) -> Result<(), Error> 
     Ok(())
 }
 
-fn compile_block(context: &Context, builder: &Block, block: &syn::Block) -> Result<(), Error> {
+fn compile_block(
+    context: &Context,
+    builder: &Block,
+    block: &syn::Block,
+    function_scope: bool,
+) -> Result<(), Error> {
     for statement in &block.stmts {
-        compile_statement(&context, builder, statement, true)?;
+        compile_statement(&context, builder, statement, function_scope)?;
     }
 
     Ok(())
@@ -48,14 +84,13 @@ fn compile_statement(
     statement: &syn::Stmt,
     function_scope: bool,
 ) -> Result<(), Error> {
-    let location = Location::unknown(context);
+    let location = Location::unknown(context.melior());
 
     match statement {
         syn::Stmt::Local(_) => todo!(),
         syn::Stmt::Item(_) => todo!(),
         syn::Stmt::Expr(expression, semicolon) => {
-            let operation = compile_expression(context, builder, expression)?;
-            let value = operation.result(0).unwrap().into();
+            let value = compile_expression(context, builder, expression)?;
 
             if semicolon.is_none() {
                 builder.append_operation(if function_scope {
@@ -75,11 +110,32 @@ fn compile_expression<'a>(
     context: &Context,
     builder: &'a Block,
     expression: &syn::Expr,
-) -> Result<OperationRef<'a>, Error> {
-    match expression {
-        syn::Expr::Lit(literal) => compile_expression_literal(context, builder, literal),
+) -> Result<Value<'a>, Error> {
+    Ok(match expression {
+        syn::Expr::Binary(operation) => compile_binary_operation(context, builder, operation)?
+            .result(0)?
+            .into(),
+        syn::Expr::Lit(literal) => compile_expression_literal(context, builder, literal)?
+            .result(0)?
+            .into(),
+        syn::Expr::Path(path) => compile_path(context, builder, path)?,
         _ => todo!(),
-    }
+    })
+}
+
+fn compile_binary_operation<'a>(
+    context: &Context,
+    builder: &'a Block,
+    operation: &syn::ExprBinary,
+) -> Result<OperationRef<'a>, Error> {
+    let location = Location::unknown(context.melior());
+    let left = compile_expression(&context, builder, &operation.left)?;
+    let right = compile_expression(&context, builder, &operation.right)?;
+
+    Ok(builder.append_operation(match &operation.op {
+        syn::BinOp::Add(_) => arith::addi(left, right, location),
+        _ => todo!(),
+    }))
 }
 
 fn compile_expression_literal<'a>(
@@ -87,25 +143,29 @@ fn compile_expression_literal<'a>(
     builder: &'a Block,
     literal: &syn::ExprLit,
 ) -> Result<OperationRef<'a>, Error> {
-    let location = Location::unknown(context);
+    let location = Location::unknown(context.melior());
 
     Ok(builder.append_operation(match &literal.lit {
         syn::Lit::Bool(boolean) => arith::constant(
-            context,
-            IntegerAttribute::new(boolean.value as i64, IntegerType::new(context, 1).into()).into(),
+            context.melior(),
+            IntegerAttribute::new(
+                boolean.value as i64,
+                IntegerType::new(context.melior(), 1).into(),
+            )
+            .into(),
             location,
         ),
         syn::Lit::Char(_) => todo!(),
         syn::Lit::Int(integer) => arith::constant(
-            context,
+            context.melior(),
             IntegerAttribute::new(
                 integer.base10_parse::<i64>()? as i64,
                 match integer.suffix() {
-                    "" => Type::index(&context),
-                    "i8" | "u8" => IntegerType::new(context, 8).into(),
-                    "i16" | "u16" => IntegerType::new(context, 16).into(),
-                    "i32" | "u32" => IntegerType::new(context, 32).into(),
-                    "i64" | "u64" => IntegerType::new(context, 64).into(),
+                    "" => Type::index(context.melior()),
+                    "i8" | "u8" => IntegerType::new(context.melior(), 8).into(),
+                    "i16" | "u16" => IntegerType::new(context.melior(), 16).into(),
+                    "i32" | "u32" => IntegerType::new(context.melior(), 32).into(),
+                    "i64" | "u64" => IntegerType::new(context.melior(), 64).into(),
                     _ => todo!(),
                 },
             )
@@ -118,6 +178,20 @@ fn compile_expression_literal<'a>(
         syn::Lit::Byte(_) => todo!(),
         _ => todo!(),
     }))
+}
+
+fn compile_path<'c>(
+    context: &Context<'c>,
+    builder: &Block,
+    path: &syn::ExprPath,
+) -> Result<Value<'c>, Error> {
+    let location = Location::unknown(context.melior());
+
+    Ok(if let Some(identifier) = path.path.get_ident() {
+        context.variable(&identifier.to_string())?
+    } else {
+        todo!()
+    })
 }
 
 #[cfg(test)]
--- mlir/src/error.rs
+++ mlir/src/error.rs
@@ -7,7 +7,9 @@ use std::{
 #[derive(Debug)]
 pub enum Error {
     AddInstruction(String),
+    Melior(melior::Error),
     Syn(syn::Error),
+    VariableNotDefined(String),
 }
 
 impl Display for Error {
@@ -16,12 +18,22 @@ impl Display for Error {
             Self::AddInstruction(name) => {
                 write!(formatter, "failed to add instruction: {}", name)
             }
+            Self::Melior(error) => write!(formatter, "{}", error),
             Self::Syn(error) => write!(formatter, "{}", error),
+            Self::VariableNotDefined(name) => {
+                write!(formatter, "variable not defined: {name}")
+            }
         }
     }
 }
 
 impl error::Error for Error {}
+
+impl From<melior::Error> for Error {
+    fn from(error: melior::Error) -> Self {
+        Self::Melior(error)
+    }
+}
 
 impl From<syn::Error> for Error {
     fn from(error: syn::Error) -> Self {
