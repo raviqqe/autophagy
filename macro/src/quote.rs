763df89d98fde28ea7b2e30b48857ed9a51b2045
Yota Toyama <raviqqe@gmail.com>
2023-06-05 10:46:13 +1000

    Fix

--- macro/src/quote.rs
+++ macro/src/quote.rs
@@ -3,11 +3,19 @@ use proc_macro::TokenStream;
 use proc_macro2::Ident;
 use quote::quote;
 use std::error::Error;
-use syn::{Expr, ExprLit, ItemFn, Lit, LitStr};
+use syn::{Expr, ExprLit, Item, ItemFn, ItemStruct, Lit, LitStr};
 
 const RAW_STRING_PREFIX: &str = "r#";
 
-pub fn generate(
+pub fn generate(attributes: &AttributeList, item: &Item) -> Result<TokenStream, Box<dyn Error>> {
+    match item {
+        Item::Fn(function) => generate_function(attributes, function),
+        Item::Struct(r#struct) => generate_struct(attributes, r#struct),
+        _ => Err("Only functions and structs can be quoted".into()),
+    }
+}
+
+fn generate_function(
     attributes: &AttributeList,
     function: &ItemFn,
 ) -> Result<TokenStream, Box<dyn Error>> {
@@ -19,7 +27,7 @@ pub fn generate(
         .map(ToOwned::to_owned)
         .unwrap_or_else(|| ident.to_string());
     let visibility = &function.vis;
-    let quote_name = Ident::new(&(ident_string + "_fn"), function.sig.ident.span());
+    let quote_name = Ident::new(&(ident_string + "_fn"), ident.span());
     let name_string = Expr::Lit(ExprLit {
         attrs: Vec::new(),
         lit: Lit::Str(LitStr::new(&ident.to_string(), ident.span())),
@@ -31,6 +39,34 @@ pub fn generate(
         }
 
         #function
+    }
+    .into())
+}
+
+fn generate_struct(
+    attributes: &AttributeList,
+    r#struct: &ItemStruct,
+) -> Result<TokenStream, Box<dyn Error>> {
+    let crate_path = parse_crate_path(attributes)?;
+    let ident = &r#struct.ident;
+    let ident_string = ident
+        .to_string()
+        .strip_prefix(RAW_STRING_PREFIX)
+        .map(ToOwned::to_owned)
+        .unwrap_or_else(|| ident.to_string());
+    let visibility = &r#struct.vis;
+    let quote_name = Ident::new(&(ident_string + "_fn"), ident.span());
+    let name_string = Expr::Lit(ExprLit {
+        attrs: Vec::new(),
+        lit: Lit::Str(LitStr::new(&ident.to_string(), ident.span())),
+    });
+
+    Ok(quote! {
+        #visibility fn #quote_name() -> #crate_path::Fn {
+            #crate_path::Fn::new(#name_string, syn::parse2(quote::quote!(#r#struct)).unwrap())
+        }
+
+        #r#struct
     }
     .into())
 }
